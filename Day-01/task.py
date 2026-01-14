de0c31ab8438ad448ba3b607f070307d18bb8d49
safin_elahi <safinelahi@gmail.com>
2026-01-15 00:36:15 +0600

    day 01 complete with a project

--- Day-01/task.py
+++ Day-01/task.py
@@ -1,1 +1,23 @@
-print("Hello")
+# task-01
+print("Hello World")
+
+# task-02
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " Angela")
+
+# task-03
+print("Hello " + input("What is your Name")+ "!")
+
+# task-04
+# task-04-01
+print(len(input("What is your name?")))
+# task-04-02
+
+username = input("What is your Name?")
+Length = len(username)
+print(Length)
+
+# task-05
+name = "Angela"
+length = len(name)
+print(length)
---
+++ Day-01/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?\n")
+print(city_name)
+user_pet_name = input("What's your pet's name?\n")
+print(user_pet_name)
+print("Your band name could be "+ city_name + " "+ user_pet_name)
