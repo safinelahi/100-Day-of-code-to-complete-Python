d43fba49786eaa69a30652d0181a974cfd2ced57
safin_elahi <safinelahi@gmail.com>
2026-01-20 02:28:08 +0600

    condition change in day-4 project . there is some problem in that condition

--- Day-04/project.py
+++ Day-04/project.py
@@ -28,27 +28,24 @@ scissors = '''
 ---.__(___)
 '''
 import random
-games_items = [rock, paper, scissors]
+game_images = [rock, paper, scissors]
 
-print("Welcome the Rock, Paper, Scissors game")
-user_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-if user_choose >= 0 or user_choose <= 2:
-    print(games_items[user_choose])
+if user_choice >= 0 and user_choice <= 2:
+    print(game_images[user_choice])
 
-computer_choose = random.randint(0,2)
+computer_choice = random.randint(0, 2)
 print("Computer chose:")
-print(games_items[computer_choose])
+print(game_images[computer_choice])
 
-if user_choose < 0 or computer_choose >= 3:
+if user_choice < 0 or user_choice > 2:
     print("You typed an invalid number. You lose!")
-elif user_choose == computer_choose:
-    print("It's a draw!")
-elif user_choose == 0 and computer_choose == 2:
+elif user_choice == computer_choice:
+    print("Draw!")
+elif (user_choice == 0 and computer_choice == 2) or \
+     (user_choice == 1 and computer_choice == 0) or \
+     (user_choice == 2 and computer_choice == 1):
     print("You win!")
-elif computer_choose == 0 and user_choose == 2:
-    print("You lose!")
-elif user_choose > computer_choose:
-    print("You win!")
-elif computer_choose > user_choose:
+else:
     print("You lose!")
